60ded2c60c4954ccaa72886bb0c057111237d434
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-06-19 18:30:23 -0500

    change jejejeeee

--- drf/settings.py
+++ drf/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = decouple_config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = decouple_config('DEBUG', default=False, cast=bool)
 
-CORS_ALLOWED_ORIGINS  = ['https://www-backend.vercel.app/']
+CORS_ALLOWED_ORIGINS  = ['www-backend.vercel.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://www-backend.vercel.app/']
 ALLOWED_HOSTS = ['https://www-backend.vercel.app/']
